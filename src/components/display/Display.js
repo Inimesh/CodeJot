4ef28cdc1c286e256a62079ac8bb2c48450b9df1
inimesh <ijmeshoulam@gmail.com>
2022-03-07 15:12:05 +0000

    Test passing: Dsiplay component has one child

---
+++ src/components/display/Display.js
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from 'react-native'
+import DisplayRender from './DisplayRender'
+
+const Display = () => {
+
+  return (
+    <DisplayRender />
+  )
+}
+
+export default Display
+
+const styles = StyleSheet.create({})
---
+++ src/components/display/DisplayRender.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const DisplayRender = ({}) => {
+  return (
+    <View>
+      <Text>DisplayRender</Text>
+    </View>
+  )
+}
+
+export default DisplayRender
+
+const styles = StyleSheet.create({})
