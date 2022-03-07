074dd3e26cd7cd6df345cb881443e6dbe8b8f9c1
inimesh <ijmeshoulam@gmail.com>
2022-03-07 16:39:10 +0000

    Test passing for DisplayRender component correctly rendering child components (snapshot)

--- src/components/display/DisplayRender.js
+++ src/components/display/DisplayRender.js
@@ -1,10 +1,10 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
-const DisplayRender = ({}) => {
+const DisplayRender = ({ text }) => {
   return (
     <View>
-      <Text></Text>
+      <Text>{text}</Text>
     </View>
   )
 }
